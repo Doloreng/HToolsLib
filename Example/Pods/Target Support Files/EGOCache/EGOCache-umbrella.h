8f6bfd25b4ba9f9dddd663760e63bd71e120b235
Eason Hong <kunpenghong@gmail.com>
2017-04-09 13:35:10 +0800

    去掉所有

--- Example/Pods/Target Support Files/EGOCache/EGOCache-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "EGOCache.h"
-
-FOUNDATION_EXPORT double EGOCacheVersionNumber;
-FOUNDATION_EXPORT const unsigned char EGOCacheVersionString[];
-
--- Example/Pods/Target Support Files/Pods-HToolsLib_Tests/Pods-HToolsLib_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_HToolsLib_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HToolsLib_TestsVersionString[];
-
